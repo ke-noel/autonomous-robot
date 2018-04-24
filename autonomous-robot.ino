72b51978ccb1046d397138ce80b44b078fb7ac1c
Kira <kira.e.noel@gmail.com>
2018-04-24 08:49:02 -0400

    more changes

--- autonomous-robot.ino
+++ autonomous-robot.ino
@@ -1,27 +1,56 @@
-#include <LIDARLite.h>
+/* LiDAR pins
+  black - GND (to Arduino GND)
+  red --- 5V (to Arduino 5V)
+  white - liDAR RX (to Arduino TX)
+  green - liDAR TX (to Arduino RX)  
+*/
 
-#define ultraEchoPinFL 0
-#define ultraTrigPinFL 1
+// Ultrasonic pins
+  #define ultraEchoPinFL 0
+  #define ultraTrigPinFL 1
 
-#define ultraEchoPinFR 2
-#define ultraTrigPinFR 3
+  #define ultraEchoPinFR 2
+  #define ultraTrigPinFR 3
 
-#define ultraEchoPinL 4
-#define ultraTrigPinL 5
+  #define ultraEchoPinL 4
+  #define ultraTrigPinL 5
 
-#define ultraEchoPinR 6
-#define ultraTrigPinR 7
+  #define ultraEchoPinR 6
+  #define ultraTrigPinR 7
 
-#define ultraEchoPinBL 8
-#define ultraTrigPinBL 9
+  #define ultraEchoPinBL 8
+  #define ultraTrigPinBL 9
 
-#define ultraEchoPinBR 10
-#define ultraTrigPinBR 11
+  #define ultraEchoPinBR 10
+  #define ultraTrigPinBR 11
 
-enum ultrasonic {FL,FR,L,R,BL,BR};
+enum ultrasonic {LL,CL,CC,CR,RR}; // L (left), C (centre), R (right)
+
+double ultraLL;
+double ultraCL;
+double ultraCC;
+double ultraCR;
+double ultraRR;
+
+volatile int liDARval;
 
 void setup() {
-  Serial.begin(9600);
+  Serial1.begin(115200); // HW serial for LiDAR
+  Serial.begin(3600); // USB to computer serial
+  delay(100);
+
+  // Set to standard output mode
+  Serial1.write(0x42);
+  Serial1.write(0x57);
+  Serial1.write(0x02);
+  Serial1.write(0x00);
+  Serial1.write(0x00);
+  Serial1.write(0x00);
+  Serial1.write(0x01);
+  Serial1.write(0x06);
+  
+  // Setup thread for reading the serial input from the LIDAR
+  // needs include library not yet installed...
   
   pinMode(ultraEchoPinFL, INPUT);
   pinMode(ultraTrigPinFL, OUTPUT);
@@ -42,22 +71,51 @@ void setup() {
   pinMode(ultraTrigPinBR, OUTPUT);
 }
 
-double getLIDAR() {
-  return 0.0; // placeholder
+double readLiDAR() {
+  // SHOULD BE IN ITS OWN THREAD
+  /* Input format for TFMini LiDAR
+    1) 0x59
+    2) 0x59
+    3) Dist_L (low 8bit)
+    4) Dist_H (high 8bit)
+    5) Strength_L (low 8bit)
+    6) Strength_H (high 8bit)
+    7) Reserved bytes
+    8) Original signal quality degree
+    9) Checksum parity bit (low 8bit), checksum = Byte1 + Byte2 + ... + Byte8 
+  */
+  if(Serial1.available()>=9) { // there should be 9 bytes of input
+    if((0x59 == Serial1.read()) && (0x59 == Serial1.read())) { // byte 1 and 2
+      unsigned int t1 = Serial1.read(); // byte 3 (Dist_L)
+      unsigned int t2 = Serial1.read(); // byte 4 (Dist_H)
+      t2 <<= 8;
+      t2 += t1;
+      liDARval = t2;
+      t1 = Serial1.read(); // byte 5 (Strength_L)
+      t2 = Serial1.read(); // byte 6 (Strength_H)
+      t2 <<= 8;
+      t2 += 1;
+      for(int i=0; i<3; i++)Serial1.read(); // byte 7, 8, 9 are ignored
+    }    
+  }
+  return liDARval;
 }
 
 double getUltrasonicDistance (double duration) {
+  /* At 21 C, v sound = 340 m/s = 0.034 cm/microsecond
+   * So, distance = (0.034cm/microsecond) * time
+   */
   return (duration / 2) / 29.1; // check value
 }
 
-double getUltrasonic(int echoPin) { // use enum
+void readUltrasonic(int echoPin) {
   long duration, distance;
   digitalWrite(echoPin + 1, LOW);
   delayMicroseconds(2);
   digitalWrite(echoPin + 1, HIGH);
   delayMicroseconds(10);
   digitalWrite(echoPin + 1, LOW);
-  duration = pulseIn(echoPin, HIGH);
+  duration = pulseIn(echoPin, HIGH); // in microseconds
   
   return getUltrasonicDistance(duration);  
 }
@@ -73,9 +131,12 @@ void stopMotors() {
 }
 
 void loop() {
-  getLIDAR();
-  for (int i = 0; i < 6; i++) {
-    getUltrasonic(2*i);
-  }
-  delay(200);
+  // check sensors
+    // check ultrasonic
+      // if anything detected by ultrasonic, prioritize ultrasonic
+        // in front? close? stop.
+        // bubble rebound algorithm
+      // otherwise prioritize liDAR
+        // 
+  delay(50);
 }
