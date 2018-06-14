db9c6fdcb3b0335d261fa5503b5e1028abd38a41
Something-Interesting <33179497+Something-Interesting@users.noreply.github.com>
2018-06-14 15:01:56 -0400

    Update autonomous-robot.ino

--- autonomous-robot.ino
+++ autonomous-robot.ino
@@ -42,8 +42,11 @@ volatile int liDARdist; // the reading from the liDAR
 #define gyroSDA 20 // must be these ports; arduino default
 #define gyroSCL 21 // must be these ports; arduino default
 
+const int MPU_addr=0x68;
 const int MPU=0x68; 
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+int minVal=265; int maxVal=402;
+double x; double y; double z;
 
 // Victor888 motor controller pins
 #define leftMotorPin 2 // PWM
@@ -111,24 +114,33 @@ void loop(){
   Wire.write(0x3B);  
   Wire.endTransmission(false);
   Wire.requestFrom(MPU,12,true);  
-  AcX=Wire.read()<<8|Wire.read();    
-  AcY=Wire.read()<<8|Wire.read();  
+  AcX=Wire.read()<<8|Wire.read();     
   AcZ=Wire.read()<<8|Wire.read();  
   GyX=Wire.read()<<8|Wire.read();  
-  GyY=Wire.read()<<8|Wire.read();  
   GyZ=Wire.read()<<8|Wire.read();  
   
   Serial.print("Accelerometer: ");
   Serial.print("X = "); Serial.print(AcX);
-  Serial.print(" | Y = "); Serial.print(AcY);
-  Serial.print(" | Z = "); Serial.println(AcZ); 
+  Serial.print(" | Y = "); Serial.println(AcZ); 
   
   Serial.print("Gyroscope: ");
   Serial.print("X = "); Serial.print(GyX);
-  Serial.print(" | Y = "); Serial.print(GyY);
-  Serial.print(" | Z = "); Serial.println(GyZ);
+  Serial.print(" | Y = "); Serial.println(GyZ);
   Serial.println(" ");
   delay(500);
+  AcX=Wire.read()<<8|Wire.read();
+  AcZ=Wire.read()<<8|Wire.read();
+  
+  int xAng = map(AcX,minVal,maxVal,-90,90);
+  int yAng = map(AcY,minVal,maxVal,-90,90);
+  int zAng = map(AcZ,minVal,maxVal,-90,90);
+
+x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI); y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI); z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
+
+Serial.print("AngleX= "); Serial.println(x);
+Serial.print("AngleY= "); Serial.println(z);
+Serial.println("-----------------------------------------");
+delay(400);
   
   //Setup motor controllers
   leftMotorSpeed = 1500;
